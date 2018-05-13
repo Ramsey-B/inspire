115090eeabcbb68f8303a2d6923bdc8dd0c2f1a5
Ramsey Bland <ramsey.bland@gmail.com>
2018-05-13 14:40:44 -0600

    fixed clock bugs

--- app/components/clock/clock.js
+++ app/components/clock/clock.js
@@ -11,10 +11,10 @@ function clock() {
     var hourPrev = hour - 1
     var hourNext = hour + 1
     var hour2 = hour + 2
-    var minCurrent = min
-    var minPrev = min - 1
-    var minNext = min + 1
-    var min2 = min + 2
+    var minCurrent = (min < 10 ? '0' : '') + min
+    var minPrev = (min < 10 ? '0' : '') + (min - 1)
+    var minNext = ((min + 1) < 10 ? '0' : '') + (min + 1)
+    var min2 = ((min + 2) < 10 ? '0' : '') + (min + 2)
 
     function alignHour(time) {
         if (time > 9) {
@@ -40,7 +40,8 @@ function clock() {
             minNext = '00'
             min2 = '01'
         } else if (time == 60) {
-            minCurrent = '01'
+            minCurrent = '00'
+            minNext = '01'
         }
     }
     changeHour(hour)
@@ -52,28 +53,18 @@ function clock() {
         ctx.clearRect(0, 0, 1000, 1000);
         ctx.font = "12vh Arial";
         ctx.fillStyle = 'yellow'
+        //current times drawn to canvas, makes them yellow to indicate current time
         ctx.fillText(hourCurrent, alignHour(hour), 110 + moveClock);
+        ctx.fillText(minCurrent, 140, 110 + moveMins)
         ctx.fillStyle = 'white'
+        //alternative times drawn to canvas
         ctx.fillText(hourPrev, alignHour(hourPrev), 190 + moveClock)
         ctx.fillText(hourNext, alignHour(hourNext), 30 + moveClock)
         ctx.fillText(hour2, alignHour(hour2), -50 + moveClock)
-        if (min < 10) {
-            ctx.fillStyle = 'yellow'
-            ctx.fillText('0' + minCurrent, 140, 110 + moveMins)
-            ctx.fillStyle = 'white'
-            ctx.fillText('0' + minPrev, 140, 190 + moveMins)
-            ctx.fillText('0' + minNext, 140, 30 + moveMins)
-            ctx.fillText('0' + min2, 140, -50 + moveMins)
-            ctx.fillText(':', 110, 110)
-        } else {
-            ctx.fillStyle = 'yellow'
-            ctx.fillText(minCurrent, 140, 110 + moveMins)
-            ctx.fillStyle = 'white'
             ctx.fillText(minPrev, 140, 190 + moveMins)
             ctx.fillText(minNext, 140, 30 + moveMins)
             ctx.fillText(min2, 140, -50 + moveMins)
             ctx.fillText(':', 110, 110)
-        }
     }
     updateClock()
 }
--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -2,15 +2,30 @@ function WeatherController(){
 	var wc = this;
 	var weatherService = new WeatherService();
 
+	var farTemp
+	var celTemp
+
+	this.changeTemp = function changeTemp(type) {
+		var template = ''
+		if(type == 'cel') {
+			template += `<h3 onclick="app.controllers.weatherController.changeTemp('far')">${celTemp}<span>&#8451;</span></h3>`
+		} else {
+			template += `<h3 onclick="app.controllers.weatherController.changeTemp('cel')">${farTemp}<span>&#8457;</span></h3>`
+		}
+		document.getElementById('temp').innerHTML = template
+	}
+
 	weatherService.getWeather(function(weather){
 		//What can you do with this weather object?
 		var kelTemp = weather.main.temp
-		var farTemp = (9/5*(kelTemp - 273) + 32).toFixed(2)
-		var celTemp = (kelTemp - 273).toFixed(2)
+		farTemp = (9/5*(kelTemp - 273) + 32).toFixed(2)
+		celTemp = (kelTemp - 273).toFixed(2)
 		var template = `
 		<div class="weather">
 			<img src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png">
-			<h3 id="temp">${farTemp}<span>&#8457;</span></h3>
+			<div id="temp">
+				<h3 onclick="app.controllers.weatherController.changeTemp('cel')">${farTemp}<span>&#8457;</span></h3>
+			</div>
 			<h4>${weather.name}</h4>
 		</div>`
 		document.getElementById('weather').innerHTML = template
