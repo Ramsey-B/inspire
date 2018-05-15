7db978be9a3499ddc9b6d76e5b95dce26db7920a
Ramsey Bland <ramsey.bland@gmail.com>
2018-05-15 10:11:11 -0600

    fixed some server and controller clock responsibilities

--- app/components/clock/clock-controller.js
+++ app/components/clock/clock-controller.js
@@ -1,3 +1,16 @@
 function ClockController() {
-    var cs = new ClockService()
+    var cs = new ClockService(drawClock)
+
+    function drawClock(previous, current, next, plus2, posIncrease, cb, context) {
+        context.fillStyle = 'yellow'
+        //current times drawn to canvas, makes them yellow to indicate current time
+        context.fillText(current, cb(current), 110 + posIncrease);
+        context.fillStyle = 'white'
+        //alternative times drawn to canvas
+        context.fillText(previous, cb(previous), 200 + posIncrease)
+        context.fillText(next, cb(next), 20 + posIncrease)
+        context.fillText(plus2, cb(plus2), -70 + posIncrease)
+        context.fillText(':', 115, 110)
+    }
+    
 }
--- app/components/clock/clock-service.js
+++ app/components/clock/clock-service.js
@@ -1,76 +1,68 @@
-function ClockService() {
+function ClockService(cb) {
     //feedback on this part of my project would be appreciated. I wanted to do something more 
     //challenging than just drawing a digital clock to the screen the screen, and I'm happy with the
     //look and functionality of the clock I made. But keeping function seperated in our normal 
     //service/controller encapsulation was a bit confusing since the function needs to be repeatedly called 
     //and still have access to the updated time and position data.
-    var date = new Date()
-    var hour = date.getHours()
-    var min = date.getMinutes()
-    var sec = date.getSeconds()
-    hour = hour % 12
-    hour = hour ? hour : 12
-    var moveClock = min * (1.5)
-    var moveMins = sec * (1.5)
-    var hourCurrent = hour
-    var hourPrev = hour - 1
-    var hourNext = hour + 1
-    var hour2 = hour + 2
-    var minCurrent = (min < 10 ? '0' : '') + min
-    var minPrev = (min < 10 ? '0' : '') + (min - 1)
-    var minNext = ((min + 1) < 10 ? '0' : '') + (min + 1)
-    var min2 = ((min + 2) < 10 ? '0' : '') + (min + 2)
+    function moveClock() {
+        var date = new Date()
+        var hour = date.getHours()
+        var min = date.getMinutes()
+        var sec = date.getSeconds()
+        hour = hour % 12
+        hour = hour ? hour : 12
+        var moveHour = min * (1.5)
+        var moveMins = sec * (1.5)
+        var hourCurrent = hour
+        var hourPrev = hour - 1
+        var hourNext = hour + 1
+        var hour2 = hour + 2
+        var minCurrent = (min < 10 ? '0' : '') + min
+        var minPrev = (min < 10 ? '0' : '') + (min - 1)
+        var minNext = ((min + 1) < 10 ? '0' : '') + (min + 1)
+        var min2 = ((min + 2) < 10 ? '0' : '') + (min + 2)
+        var posCurrent = 110
 
-    function alignHour(time) {
-        if (time > 9) {
-            return 5
-        } else {
-            return 60
+        function alignHour(time) {
+            if (time > 9) {
+                return 5
+            } else {
+                return 60
+            }
         }
-    }
-
-    function changeHour(time) {
-        if (time == 11) {
-            hour2 = 1
-        } else if (time == 12) {
-            hourNext = 1
-            hour2 = 2
+        function alignMin() {
+            return 140
         }
-    }
 
-    function changeMin(time) {
-        if (time == 58) {
-            min2 = '00'
-        } else if (time == 59) {
-            minNext = '00'
-            min2 = '01'
-        } else if (time == 60) {
-            minCurrent = '00'
-            minNext = '01'
+        function changeHour(time) {
+            if (time == 11) {
+                hour2 = 1
+            } else if (time == 12) {
+                hourNext = 1
+                hour2 = 2
+            }
         }
-    }
-    changeHour(hour)
-    changeMin(min)
 
-    function updateClock() {
+        function changeMin(time) {
+            if (time == 58) {
+                min2 = '00'
+            } else if (time == 59) {
+                minNext = '00'
+                min2 = '01'
+            } else if (time == 60) {
+                minCurrent = '00'
+                minNext = '01'
+            }
+        }
+        changeHour(hour)
+        changeMin(min)
+
         var canvas = document.getElementById("clock");
         var ctx = canvas.getContext("2d")
         ctx.clearRect(0, 0, 1000, 1000);
         ctx.font = "12vh sans-serif";
-        ctx.fillStyle = 'yellow'
-        //current times drawn to canvas, makes them yellow to indicate current time
-        ctx.fillText(hourCurrent, alignHour(hour), 110 + moveClock);
-        ctx.fillText(minCurrent, 140, 110 + moveMins)
-        ctx.fillStyle = 'white'
-        //alternative times drawn to canvas
-        ctx.fillText(hourPrev, alignHour(hourPrev), 200 + moveClock)
-        ctx.fillText(hourNext, alignHour(hourNext), 20 + moveClock)
-        ctx.fillText(hour2, alignHour(hour2), -70 + moveClock)
-            ctx.fillText(minPrev, 140, 200 + moveMins)
-            ctx.fillText(minNext, 140, 20 + moveMins)
-            ctx.fillText(min2, 140, -70 + moveMins)
-            ctx.fillText(':', 115, 110)
+        cb(hourPrev, hourCurrent, hourNext, hour2, moveHour, alignHour, ctx)
+        cb(minPrev, minCurrent, minNext, min2, moveMins, alignMin, ctx)
     }
-    updateClock()
+    setInterval(moveClock, 500)
 }
-setInterval(ClockService, 500)
