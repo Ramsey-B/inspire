89c09f5cb60da2c0a4af018d432b729bcea81eaf
Ramsey Bland <ramsey.bland@gmail.com>
2018-06-12 22:34:12 -0600

    cleaned up for portfollio

--- app/components/quote/quote-controller.js
+++ app/components/quote/quote-controller.js
@@ -4,7 +4,7 @@ function QuoteController(){
 
 	qs.getQuote(function(quote){
 		var template = `
-		<h5 class="quote-text">${quote.quote}</h5>
+		<h5 class="quote-text"><i class="fas fa-quote-left"></i> ${quote.quote}</h5>
 		<p class="author text-center">-${quote.author}</p>`
 		document.getElementById('quote').innerHTML = template
 	})
